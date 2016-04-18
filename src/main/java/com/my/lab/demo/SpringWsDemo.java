8a4839ae8a7539c25422e6ed35220466868a93ad
harumamburu <nikolay.kovalev92@gmail.com>
2016-04-18 17:48:38 +0300

    Initial commit

---
+++ src/main/java/com/my/lab/demo/SpringWsDemo.java
@@ -0,0 +1,12 @@
+package com.my.lab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWsDemo.class, args);
+    }
+}
